2ca6896ea5a0d367bf7c3de2a732fd60e7c74404
jjasae777-source <jjasae777@gmail.com>
2025-11-06 07:16:33 +0000

    코드 수정 2 # Please enter the commit message for your changes. Lines starting

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("🎈 SEHOON app YES")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "oh yes! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
